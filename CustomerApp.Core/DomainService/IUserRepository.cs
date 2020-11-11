c920f6347eef4aa722487e86ea3b3fb0e4e214ef
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2020-11-11 19:06:32 +0100

    Update IUserRepository.cs

--- CustomerApp.Core/DomainService/IUserRepository.cs
+++ CustomerApp.Core/DomainService/IUserRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace CustomerApp.Core.DomainService
-{/
+{
     public interface IUserRepository<T>
     {
         IEnumerable<T> GetAll();
